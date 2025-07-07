f68452e3b2c44d6d9b387e196db2203d3c2e7fd6
RosyPetrella <petrellamr@gmail.com>
2025-07-07 11:45:02 +0200

    Add functionality to manage cart items and display added products

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,62 @@ const products = [
   { name: "Pasta", price: 0.7 },
 ];
 
+// 📌 Milestone 2: Aggiungere prodotti al carrello
+// Aggiungi uno stato locale addedProducts (inizialmente un array vuoto) per rappresentare i prodotti nel carrello.
+// Per ogni prodotto della lista, aggiungi un bottone "Aggiungi al carrello":
+// Al click del bottone, usa una funzione addToCart per:
+// Aggiungere il prodotto al carrello se non è già presente, con una proprietà quantity = 1.
+// Se il prodotto è già nel carrello, ignora l’azione.
+// Sotto alla lista dei prodotti, mostra una lista dei prodotti nel carrello se addedProducts contiene almeno un elemento.
+// Per ogni prodotto nel carrello, mostra:
+// Nome
+// Prezzo
+// Quantità
+
+// Obiettivo: L’utente può aggiungere prodotti al carrello e vedere una lista dei prodotti aggiunti.
+
 function App() {
+  const [addedProducts, setaddedProducts] = useState([]);
+
+  function addToCart(product) {
+    const exists = addedProducts.find((p) => p.name === product.name);
+    if (!exists) {
+      setaddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+    }
+  }
   return (
     <>
       <h1>Prodotti</h1>
       <ul>
         {products.map((product, index) => {
           return (
-            <li key={index}>
-              {product.name}, € {product.price}
-            </li>
+            <div key={index}>
+              <li>
+                {product.name}, € {product.price}
+              </li>
+              <button
+                onClick={() => {
+                  addToCart(product);
+                }}
+              >
+                Aggiungi al carrello
+              </button>
+            </div>
           );
         })}
       </ul>
+      <h1>Carrello</h1>
+      {addedProducts.length > 0 && (
+        <ul>
+          {addedProducts.map((prod, index) => (
+            <li key={index}>
+              {prod.name}, € {prod.price}, {prod.quantity}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
 
 export default App;
-// Crea un componente che mostra la lista dei prodotti.
-// Per ogni prodotto, mostra:
-// Nome
-// Prezzo
-// Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.
